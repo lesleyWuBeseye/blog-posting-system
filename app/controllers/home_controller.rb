36e5b3b3fab1eda58085a44b55cf166c7721ef09
lesley <lesley@yunshouhuxingzhengde-MacBook-Pro.local>
2021-08-04 16:15:01 +0800

    Add home#index as landing page route - Add home#index page - Add 'Show All Users' button to users#index page - Add 'Show All Boards' button to board#index page

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Controll landing page action
+class HomeController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  resources :users
+  resources :boards
 end
